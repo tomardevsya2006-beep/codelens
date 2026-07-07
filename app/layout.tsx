2c3573cbc7a122b06b502f47bc471e8ef347aff5
tomardevsya2006-beep <tomardevsya2006@gmail.com>
2026-07-07 15:04:17 +0530

    fix: use relative path for layout utility import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Figtree } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
+import { cn } from "../lib/utils";
 
 const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 
